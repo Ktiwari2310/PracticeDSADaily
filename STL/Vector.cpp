3598069426fe8920ad9898cb7f14c659ffab64d5
Ktiwari2310 <beingkt.23@gmail.com>
2025-08-05 23:51:18 +0530

    Added vector

--- STL/Vector.cpp
+++ STL/Vector.cpp
@@ -1,1 +1,12 @@
-
+#include<vector>
+#include<iostream>
+using namespace std;
+void main(){
+	vector <int> v;
+	v.push_back(2);
+	v.emplace_back(3);
+	for ( auto it : v){
+		cout<<(it)<<"  ";
+	}
+}
+//output 2  3
